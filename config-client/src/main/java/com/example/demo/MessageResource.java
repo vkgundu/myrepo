d283b80816e74c806fed393faeb074554647ae41
Vinay Kumar Gundu <vgund01@safeway.com>
2019-11-04 23:35:54 -0800

    added config servers projects

---
+++ config-client/src/main/java/com/example/demo/MessageResource.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author vgund01
+ *
+ */
+@RefreshScope
+@RequestMapping("/rest")
+@RestController
+public class MessageResource {
+	
+	@Value("${message: Default Hello}")
+	private String message;
+	
+	@GetMapping("/message")
+	public String message() {
+		return message;
+	}
+
+}
